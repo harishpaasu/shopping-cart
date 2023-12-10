22f457b2c715943d9b105608615e188c9acdb9b8
harish paasu <paasuharish6@gmail.com>
2023-12-11 00:54:45 +0530

    first commit

---
+++ shopping-task/src/Data.jsx
@@ -0,0 +1,44 @@
+import img1 from"./img/adidas.png"
+import img2 from"./img/airjordan.png"
+import img3 from"./img/nike.png"
+import img4 from"./img/puma.png"
+
+ const data = {
+    productData:[
+        {
+            id:1,
+            img:img1,
+            title:"Adidas shoes",
+            desc:"Adidas shoes are celebrated for their cutting-edge features, including advanced cushioning technologies, lightweight materials, and ergonomic designs that enhance performance and comfort. Whether it's the classic Stan Smiths, the energy-returning Boost technology. ",
+            price:3999,
+        },
+
+        {
+            id:2,
+            img:img2,
+            title:"Airjordan shoes",
+            desc:"Each pair of Air Jordans tells a story, with numerous editions released over the years, each connected to a specific chapter in Michael Jordan's illustrious career. The shoes are not only a tribute to basketball greatness but also a reflection of the cultural impact Jordan has had on sports and fashion.",
+            price:4999,
+        },
+
+        {
+            id:3,
+            img:img3,
+            title:"Nike shoes",
+            desc:"Nike's commitment to sustainability is reflected in initiatives like the use of recycled materials and eco-friendly manufacturing processes. This dedication to environmental responsibility aligns with the brand's broader ethos of innovation and social responsibility.",
+            price:2999,
+        },
+
+
+        {
+            id:4,
+            img:img4,
+            title:"Puma shoes",
+            desc:"Puma shoes are characterized by their sleek designs, technological advancements, and a commitment to blending functionality with fashion. Renowned for their performance-oriented features, such as responsive cushioning, durable outsoles, and lightweight materials.",
+            price:2599,
+        },
+
+    ],
+ };
+
+ export default data;
